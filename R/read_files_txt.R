aaa92a40dc91e8cf3df2d94ae802356172d8f443
claudehspencer <claudespencer111@gmail.com>
2023-11-14 08:23:30 +0800

    test update to read_files_txt function

--- R/read_files_txt.R
+++ R/read_files_txt.R
@@ -6,10 +6,20 @@
 #' @export
 #'
 #' @examples
-read_files_txt <- function(flnm) {
-  read_tsv(flnm, col_types = cols(.default = "c")) %>%
+read_files_txt <- function(dir) {
+  
+  read_dat <- function(flnm){
+    read_tsv(flnm, col_types = cols(.default = "c")) %>%
     dplyr::mutate(campaignid = basename(flnm)) %>%
     clean_names() %>%
     dplyr::mutate(campaignid = str_replace_all(campaignid,c("_Points.txt"="","_3DPoints.txt"="","_Lengths.txt"=""))) %>%
     dplyr::rename(sample = opcode)
+  }
+  
+  list.files(path = dir,      
+             recursive = F,
+             pattern = "_Metadata.csv",
+             full.names = T) %>%
+    purrr::map(~read_dat(.)) %>%
+    purrr::list_rbind()
 }
