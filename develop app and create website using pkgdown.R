8c8221848e81a5be2dddb303dc9f251a9a5afc2e
BrookeGibbons <21301367@student.uwa.edu.au>
2023-11-17 14:59:55 +0800

    pkgdown script

--- develop app and create website using pkgdown.R
+++ develop app and create website using pkgdown.R
@@ -35,3 +35,14 @@ pkgdown::build_site()
 
 # Turn into a github page (need to commit and push changes for it to show up)
 usethis::use_pkgdown_github_pages() # CHOOSE NO TO OVERWRITING THE YML FILE
+
+
+
+
+# PACKAGE VERSIONS
+#> What should the new version be? 
+#> 
+#> 1: major --> 1.0
+#> 2: minor --> 0.2
+#> 3: patch --> 0.1.1
+#> 4:   dev --> 0.1.0.9000
