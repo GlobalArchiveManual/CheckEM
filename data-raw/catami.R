a7365d272e8a17a4ece0c32753ca49dc24956cee
claudehspencer <claudespencer111@gmail.com>
2023-11-29 07:52:53 +0800

    adding datasets to the package. scripts used to create datasets should be ignored from the package build?

---
+++ data-raw/catami.R
@@ -0,0 +1,17 @@
+## Code to prepare `catami` dataset ----
+# This dataset contains a tidy version of the CATAMI Classification Scheme for scoring marine biota and substrata in underwater imagery
+# See here - https://catami.org/
+
+library(googlesheets4)
+library(usethis)
+library(tidyverse)
+library(CheckEM)
+
+url <- "https://docs.google.com/spreadsheets/d/1tcvHnD8LtPmjro8eOMdOS6k6HKdND86mMAOk6AS_gfc/edit?usp=sharing"
+
+catami <- googlesheets4::read_sheet(url, sheet = "code crosswalk") %>%
+  CheckEM::clean_names() %>%
+  dplyr::select(starts_with("level"), family, genus, species, qualifiers) %>%
+  glimpse()
+
+usethis::use_data(catami, overwrite = TRUE)
---
+++ data-raw/maturity.R
@@ -0,0 +1,17 @@
+## Code to prepare `maturity` dataset ----
+# This dataset contains length of maturity data for common West Australian recreationally and commercially targeted species
+# The vast majority of this data has been obtained from Department of Fisheries public reports
+# See here - https://www.fish.wa.gov.au/Documents/management_papers/fmp280.pdf
+
+library(googlesheets4)
+library(usethis)
+library(tidyverse)
+library(CheckEM)
+
+url <- "https://docs.google.com/spreadsheets/d/176genWqd_pc3NDVQtP2CmfMh66Ui6uwWrLoJU1Ny_qw/edit?usp=sharing"
+
+maturity <- googlesheets4::read_sheet(url, sheet = "fisheries Lm") %>%
+  CheckEM::clean_names() %>%
+  glimpse()
+
+usethis::use_data(maturity, overwrite = TRUE)
