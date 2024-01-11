6931652dab118d4e66c41d87a8c749e3d55a80ed
BrookeGibbons <21301367@student.uwa.edu.au>
2024-01-11 14:24:29 +0800

    add global data to the package

--- data-raw/life-history.R
+++ data-raw/life-history.R
@@ -6,3 +6,7 @@ library(CheckEM)
 australia_life_history <- readRDS("annotation-schema/data/tidy/simple-life-history.RDS")
 
 usethis::use_data(australia_life_history, overwrite = TRUE)
+
+global_life_history <- readRDS("annotation-schema/data/tidy/global_fish.life.history.RDS")
+
+usethis::use_data(global_life_history, overwrite = TRUE)
