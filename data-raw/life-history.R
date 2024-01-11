afa16605928bd5584bfade94cecc4aab79075d98
BrookeGibbons <21301367@student.uwa.edu.au>
2024-01-11 13:36:31 +0800

    add life history data to CheckEM package

---
+++ data-raw/life-history.R
@@ -0,0 +1,8 @@
+## Code to prepare australia and global life history datasets ----
+library(usethis)
+library(tidyverse)
+library(CheckEM)
+
+australia_life_history <- readRDS("annotation-schema/data/tidy/simple-life-history.RDS")
+
+usethis::use_data(australia_life_history, overwrite = TRUE)
