d8b485437bedbaced5a486ad63e7c7e84f1abd80
BrookeGibbons <21301367@student.uwa.edu.au>
2024-02-01 15:09:52 +0800

    change marine region to sf package instead of rgeos

--- data-raw/spatial.R
+++ data-raw/spatial.R
@@ -6,7 +6,7 @@ wgs_84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
 aus_regions <- readOGR(dsn = "C:/GitHub/Files to big for CheckEM/spatial/marine_regions.shp")
 aus_regions$REGION <- as.character(aus_regions$REGION)
-proj4string(aus_regions) <- CRS(wgs_84)
+# proj4string(aus_regions) <- CRS(wgs_84)
 
 world_regions <- readOGR(dsn = "C:/GitHub/Files to big for CheckEM/spatial/FAO_major_areas.shp")
 world_regions$REGION <- str_replace_all(world_regions$NAME_EN, c(", " = "_", " " ="."))
