3d36c278f6e2f4cce569f3eabf2e1947b43271ee
Brooke Gibbons <00097191@uwa.edu.au>
2024-08-23 13:34:50 +0800

    clean up

--- temp functions/example on using token.R
+++ temp functions/example on using token.R
@@ -3,11 +3,11 @@
 #3. Create script in secrets folder that creates token.rda
 #4. Load in token.rda in API call script.
 
-# In secret script
-token <- "123456"
-save(token, file = "secrets/token.rda")
-
-# In API script
-load("secrets/token.rda")
-
-metadata <- ga_api_metadata(token = token, synthesis_id = 14)
+# # In secret script
+# token <- "123456"
+# save(token, file = "secrets/token.rda")
+# 
+# # In API script
+# load("secrets/token.rda")
+# 
+# metadata <- ga_api_metadata(token = token, synthesis_id = 14)
