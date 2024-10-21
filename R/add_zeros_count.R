5519945ace0c2eef4d6d118066da124af49f4a41
BrookeGibbons <brooke.gibbons@uwa.edu.au>
2024-10-21 15:29:44 +0800

    fix mistakes in count data

--- R/add_zeros_count.R
+++ R/add_zeros_count.R
@@ -16,6 +16,7 @@ add_zeros_count <- function(count_data, metadata) {
   
   # Process count data into a wide format
   count_with_zeros <- count_data %>%
+    dplyr::select(sample_url, family, genus, species, count) %>%
     dplyr::full_join(count_metadata, by = "sample_url") %>%
     dplyr::filter(successful_count == TRUE) %>%
     dplyr::select(campaignid, sample, family, genus, species, count) %>%
@@ -24,8 +25,8 @@ add_zeros_count <- function(count_data, metadata) {
     dplyr::group_by(campaignid, sample, family, genus, species) %>%
     dplyr::summarise(count = sum(count)) %>%
     dplyr::mutate(scientific_name = paste(family, genus, species, sep = " ")) %>%
-    dplyr::select(campaignid, sample, scientific_name, count) %>%
-    dplyr::full_join(count_metadata, by = "sample_url") %>%
+    dplyr::select(campaignid, sample, scientific_name, family, genus, species, count) %>%
+    dplyr::full_join(count_metadata) %>%
     dplyr::filter(successful_count == TRUE) 
   
   return(count_with_zeros)
