bcd342cab1d9dd50266d2251c80b84716b316420
BrookeGibbons <brooke.gibbons@uwa.edu.au>
2024-12-04 14:55:04 +0800

    add plots to git

---
+++ manuscript/r/EventMeasure-users-plot.R
@@ -0,0 +1,30 @@
+# Load necessary libraries
+library(ggplot2)
+library(dplyr)
+library(readxl)
+library(CheckEM)
+
+# Read in EM data ----
+data <- read_xlsx("manuscript/data/eventmeasure_users_2008-11_2024.xlsx") %>%
+  CheckEM::clean_names()
+
+# Create the plot
+em_users <- ggplot(data, aes(x = year)) +
+  geom_line(aes(y = au_cumulative, color = "Australia"), size = 1) +
+  geom_line(aes(y = int_cumulative, color = "International"), size = 1) +
+  labs(x = "Year", y = "Cumulative Number of EventMeasure Licences") +
+  scale_color_manual(values = c("Australia" = "#1f77b4", "International" = "#ff7f0e")) + # Updated colors
+  scale_x_continuous(breaks = seq(2008, 2024, by = 2)) +  # Increased number of year breaks
+  theme_minimal() +
+  theme(
+    legend.title = element_blank(),
+    axis.line = element_line(color = "black", size = 0.5),
+    text = element_text(family = "Times New Roman", size = 12)
+  )
+
+em_users
+
+ggsave("manuscript/plots/event_measure_licences_plot.png", 
+       plot = em_users, device = "png", 
+       width = 7.27,                       # Width of A4 page in inches
+       height = 4, units = "in", dpi = 300)
--- manuscript/r/map-for-CheckEM-manuscript.R
+++ manuscript/r/map-for-CheckEM-manuscript.R
@@ -19,7 +19,7 @@ publication_theme <- theme_classic() +
   )
 
 # Load the dataset (replace 'data-export.csv' with your actual file path)
-data <- read_csv("data-export.csv", skip = 9) %>%
+data <- read_csv("manuscript/data/data-export.csv", skip = 9) %>%
   select(Country, `Active users`) %>%
   rename(active_users = `Active users`) %>%
   filter(!is.na(active_users)) %>%
@@ -58,7 +58,8 @@ unmatched
 
 # Match country names and merge data
 world_data <- world %>%
-  left_join(data, by = c("region" = "Country"))
+  left_join(data, by = c("region" = "Country")) %>%
+  dplyr::filter(!region %in% "Antarctica")
 
 # Prepare data for labels (mean longitude and latitude for each region)
 label_data <- world_centerpoints %>%
@@ -66,28 +67,70 @@ label_data <- world_centerpoints %>%
   filter(!is.na(active_users))
 
 # Plot the map
-ggplot() +
+map <- ggplot() +
   # Add world polygons with gradient fill
+  # geom_sf(data = world_data_sf,
+  #              aes(fill = active_users), 
+  #              # color = "black",
+  #              size = 0.01) +  # Thin black outlines
+  
   geom_polygon(data = world_data,
-               aes(long, lat, group = group, fill = active_users), 
-               # color = "black",
-               size = 0.01) +  # Thin black outlines
+               aes(long, lat, group = group, fill = active_users),
+               color = "white",
+               size = 0.3) +  # Thin black outlines
   
   # Add labels at country centroids
   geom_text(data = label_data,
             aes(label = active_users, geometry = geometry),
-            stat = "sf_coordinates", size = 3, color = "black", fontface = "bold") +  # Bigger text
+            stat = "sf_coordinates", size = 3, color = "black", fontface = "bold",
+            family = "Times New Roman") +  # Bigger text
   
   # Gradient fill from light blue to dark blue
-  scale_fill_gradient(low = "#A7BCE0", high = "#263F6B", na.value = "grey", name = "Number of Users") +
+  scale_fill_gradient(low = "#A7BCE0", 
+                      high = "#335591", 
+                      na.value = "grey", 
+                      name = "Number of Users",
+                      breaks = c(1, 150, 300, 450, 600),  # Specify breaks
+                      labels = c("1", "150", "300", "450", "600"))  +# Optionally customize labels
   
   
+  # Crop the plot to remove areas below -55 latitude
+  # coord_sf(ylim = c(-55, 90))  + # Set latitude range from -55 to 90
+  
+  # scale_y_continuous(breaks = c(-50, 0, 50)) + 
   # Maintain aspect ratio
-  coord_fixed(ratio = 1) + 
+  # coord_sf(default_crs = sf::st_crs(4326)) +
+  
+  # scale_y_continuous(breaks = c(-50, 0, 50)) + 
+  # scale_x_continuous(breaks = seq(-84, -76, by = 1)) +
+
+scale_y_continuous(breaks = c(-50, -25, 0, 25, 50, 75), labels = c("50°S", "25°S", "0°", "25°N", "50°N", "75°N")) +
+  scale_x_continuous(breaks = c(-120, -60, 0, 60, 120), labels = c("120°W", "60°W", "0°", "60°E", "120°E"))+
+  coord_fixed(ratio = 1, xlim = c(-155, 170)) +
   labs(
-    title = "CheckEM users",
+    # title = "CheckEM users",
     x = "Longitude",
     y = "Latitude"
   ) +
-  publication_theme
-  
+  publication_theme +
+  theme(
+    panel.grid.major = element_line(color = "gray80"),  # Add grid lines for both axes
+    axis.text = element_text(size = 10),               # Make axis text visible
+    axis.title = element_blank(),                      # Remove axis titles
+    axis.ticks = element_line(size = 0.5),              # Add axis ticks
+    legend.position = "bottom",            # Move legend below the plot
+    text = element_text(family = "Times New Roman", size = 12)
+  ) 
+
+map
+
+# Save the plot as a PNG with A4 width and adjusted height
+ggsave(
+  filename = "manuscript/plots/map_plot_a4.png",  # File name
+  plot = map,                 # Use the last plotted ggplot object
+  device = "png",                     # Save as PNG
+  dpi = 300,                          # High resolution for publication
+  width = 7.27,                       # Width of A4 page in inches
+  height = 4.5,                         # Adjust height to fit the plot nicely
+  units = "in"                        # Specify dimensions in inches
+)
